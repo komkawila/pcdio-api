115aec285644269b3ca36bc3beca8af1b4bc1e1f
komkawila <kompanuwat@gmail.com>
2021-12-25 15:17:59 +0700

    edit /datalog4

--- app.js
+++ app.js
@@ -24,9 +24,6 @@ app.use('/login', loginRoute);
 
 const datalogRoute = require('./routes/datalog/datalog');
 app.use('/datalog', datalogRoute);
-app.use('/datalog2', datalogRoute);
-app.use('/datalog3', datalogRoute);
-app.use('/datalog4', datalogRoute);
 
 app.listen(port, () =>
     console.log(`Example app listening on port ${port}!`)

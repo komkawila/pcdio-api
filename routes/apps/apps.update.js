fed550621f0375aee0facf39005a3ed3cefcba63
komkawila <kompanuwat@gmail.com>
2022-02-02 09:07:13 +0700

    add sensor2 api

--- routes/apps/apps.update.js
+++ routes/apps/apps.update.js
@@ -2,6 +2,7 @@ const express = require('express');
 const db = require('../../util/db.config');
 const route = express.Router();
 
+// SENSOR 1
 route.patch('/countset/:device_id', async (req, res, next) => {
     const device_id = req.params.device_id;
     const value = req.body.value;
@@ -50,4 +51,52 @@ route.patch('/countlast/:device_id', async (req, res, next) => {
         });
 });
 
+// SENSOR 2
+route.patch('/countset2/:device_id', async (req, res, next) => {
+    const device_id = req.params.device_id;
+    const value = req.body.value;
+    await db.query("UPDATE device_tb SET count2_set= ? WHERE device_id = ?",
+        [value, device_id],
+        function (err, result, fields) {
+            if (err) {
+                console.log(err);
+                res.send({ err: true, status: false, message: err });
+            } else {
+                const json = { err: false, status: (result.length == 0 ? false : true), message: result };
+                res.send(json);
+            }
+        });
+});
+
+route.patch('/count2/:device_id', async (req, res, next) => {
+    const device_id = req.params.device_id;
+    const value = req.body.value;
+    await db.query("UPDATE device_tb SET count2 = ? WHERE device_id = ?",
+        [value, device_id],
+        function (err, result, fields) {
+            if (err) {
+                console.log(err);
+                res.send({ err: true, status: false, message: err });
+            } else {
+                const json = { err: false, status: (result.length == 0 ? false : true), message: result };
+                res.send(json);
+            }
+        });
+});
+
+route.patch('/countlast2/:device_id', async (req, res, next) => {
+    const device_id = req.params.device_id;
+    const value = req.body.value;
+    await db.query("UPDATE device_tb SET count2_last= ? WHERE device_id = ?",
+        [value, device_id],
+        function (err, result, fields) {
+            if (err) {
+                console.log(err);
+                res.send({ err: true, status: false, message: err });
+            } else {
+                const json = { err: false, status: (result.length == 0 ? false : true), message: result };
+                res.send(json);
+            }
+        });
+});
 module.exports = route;

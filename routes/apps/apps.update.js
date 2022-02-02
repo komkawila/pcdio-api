907de24888b605f77184324d550c1e80129368a3
komkawila <kompanuwat@gmail.com>
2022-02-02 09:20:47 +0700

    add [PATCH] /setsensor

--- routes/apps/apps.update.js
+++ routes/apps/apps.update.js
@@ -99,4 +99,21 @@ route.patch('/countlast2/:device_id', async (req, res, next) => {
             }
         });
 });
+
+// SETSENSOR ตั้งค่าเปิดรีเลย์ตามจำนวนเซนเซอร์
+route.patch('/setsensor/:device_id', async (req, res, next) => {
+    const device_id = req.params.device_id;
+    const value = req.body.value;
+    await db.query("UPDATE device_tb SET setsensor= ? WHERE device_id = ?",
+        [value, device_id],
+        function (err, result, fields) {
+            if (err) {
+                console.log(err);
+                res.send({ err: true, status: false, message: err });
+            } else {
+                const json = { err: false, status: (result.length == 0 ? false : true), message: result };
+                res.send(json);
+            }
+        });
+});
 module.exports = route;

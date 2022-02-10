b4695dcb892156f5c5cf78a40d2a482f6255d939
komkawila <kompanuwat@gmail.com>
2022-02-10 15:47:31 +0700

    add setbath&settimerelayC

--- routes/apps/apps.update.js
+++ routes/apps/apps.update.js
@@ -132,4 +132,36 @@ route.patch('/setsensor2/:device_id', async (req, res, next) => {
             }
         });
 });
+// settime_relayC ตั้งค่าเปิดรีเลย์ตามจำนวนเซนเซอร์
+route.patch('/settimerelayc/:device_id', async (req, res, next) => {
+    const device_id = req.params.device_id;
+    const value = req.body.value;
+    await db.query("UPDATE device_tb SET settime_relayC= ? WHERE device_id = ?",
+        [value, device_id],
+        function (err, result, fields) {
+            if (err) {
+                console.log(err);
+                res.send({ err: true, status: false, message: err });
+            } else {
+                const json = { err: false, status: (result.length == 0 ? false : true), message: result };
+                res.send(json);
+            }
+        });
+});
+// settime_relayC ตั้งค่าเปิดรีเลย์ตามจำนวนเซนเซอร์
+route.patch('/setbath/:device_id', async (req, res, next) => {
+    const device_id = req.params.device_id;
+    const value = req.body.value;
+    await db.query("UPDATE device_tb SET setbath= ? WHERE device_id = ?",
+        [value, device_id],
+        function (err, result, fields) {
+            if (err) {
+                console.log(err);
+                res.send({ err: true, status: false, message: err });
+            } else {
+                const json = { err: false, status: (result.length == 0 ? false : true), message: result };
+                res.send(json);
+            }
+        });
+});
 module.exports = route;

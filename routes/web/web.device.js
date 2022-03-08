e1f1d34bc301f6cb5fb85a2537ecd73a4ce55bec
komkawila <kompanuwat@gmail.com>
2022-03-08 23:12:09 +0700

    1

---
+++ routes/web/web.device.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const db = require('../../util/db.config');
+const route = express.Router();
+
+// GET Devices
+route.get('/', async (req, res, next) => {
+    const device_name = req.body.device_name;
+    await db.query("SELECT * FROM device_tb WHERE device_name = ?",
+        [device_name], function (err, result, fields) {
+            if (err) {
+                console.log(err);
+                res.send({ err: true, status: false, message: err });
+            } else {
+                const json = { err: false, status: (result.length == 0 ? false : true), message: result };
+                res.send(json);
+            }
+        });
+});
+
+module.exports = route;
--- routes/web/web.js
+++ routes/web/web.js
@@ -4,4 +4,7 @@ const route = express.Router();
 const loginRoute = require('./web.login');
 route.use('/login', loginRoute);
 
+const deviceRoute = require('./web.device');
+route.use('/device', deviceRoute);
+
 module.exports = route;

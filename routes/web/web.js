24b2b77ebea9b7f02b92bc6074396b8ee1ff0320
komkawila <kompanuwat@gmail.com>
2022-03-08 17:05:12 +0700

    1

--- routes/web/web.js
+++ routes/web/web.js
@@ -2,6 +2,6 @@ const express = require('express');
 const route = express.Router();
 
 const loginRoute = require('./web.login');
-route.use('/login', dayRoute);
+route.use('/login', loginRoute);
 
 module.exports = route;

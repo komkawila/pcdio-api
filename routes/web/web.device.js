0214d5e68800dc8d11bd51426dd015795c9647ba
komkawila <kompanuwat@gmail.com>
2022-03-08 23:13:21 +0700

    1

--- routes/web/web.device.js
+++ routes/web/web.device.js
@@ -3,7 +3,7 @@ const db = require('../../util/db.config');
 const route = express.Router();
 
 // GET Devices
-route.get('/', async (req, res, next) => {
+route.get('/:device_name', async (req, res, next) => {
     const device_name = req.params.device_name;
     await db.query("SELECT * FROM device_tb WHERE device_name = ?",
         [device_name], function (err, result, fields) {
